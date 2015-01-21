69955c0a97b945d7bb00eba9e104e42eadce45a2
smitapandy <pandey.aasmita@gmail.com>
2015-01-21 22:29:42 +0530

    code for carmodal

---
+++ carmodel.py
@@ -0,0 +1,24 @@
+class Car:
+	def__init__(self):
+		self.modal = "abc"
+		
+	def modal():
+		print "model"
+	
+class Honda(car):
+
+	def color():
+		print "color"
+		
+	def wheel_modal():
+	def speed():
+	def engine_power():
+	def mileage():
+	
+	
+class Honda_CITY(Honda):
+	pass
+	
+
+	
+	
